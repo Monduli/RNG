ca263dc73ae00577377c81c9a91973a0b55aea2b
Dan <46987754+Monduli@users.noreply.github.com>
2020-12-29 17:02:19 -0500

    put name/disclaimer on

--- rng.py
+++ rng.py
@@ -1,4 +1,10 @@
+#This program was written by @Monduli
+#Stealing it doesn't benefit anyone as it was not difficult to make
+#Just make it yourself if you're going to bother stealing it
+#At least then you can learn a tiny bit about tkinter
+
 import tkinter as tk
+from tkinter.ttk import *
 import random
 
 class Application(tk.Frame):
@@ -16,11 +22,13 @@ class Application(tk.Frame):
 		tk.Label(self._master, text="Number 1").grid(row=0)
 		tk.Label(self._master, text="Number 2").grid(row=1)
 		self.to_set = tk.Label(self._master, text=self.result_text)
+		self.to_set.grid(row=2, column=1)
 		self.text1 = tk.Entry(self._master)
 		self.text1.grid(row=0, column=1)
 		self.text2 = tk.Entry(self._master)
 		self.text2.grid(row=1, column=1)
 		self.button = tk.Button(text='Pick a number!', command=self.result).grid(row=2, column=0)
+
 
 	def result(self):
 		"""
@@ -28,16 +36,16 @@ class Application(tk.Frame):
 		"""
 		x = int(self.text1.get())
 		y = int(self.text2.get())
-		print(x, y)
 		result = random.randint(x, y)
 		self.result_text = "Result: " + str(result) + "."
-		self.to_set = tk.Label(self._master, text=self.result_text, padx=6)
-		self.to_set.grid(row=2, column=1)
+		self.to_set['text'] = self.result_text
 
 #create window
 #with two input spaces
 #with text "Please enter 2 numbers. A number in this range will be produced."
 window = tk.Tk()
+window.title("RNG")
+window.iconbitmap("free-d8.ico")
 canvas = tk.Canvas(window, width=400, height=300)
 app = Application(window)
 app.mainloop()
